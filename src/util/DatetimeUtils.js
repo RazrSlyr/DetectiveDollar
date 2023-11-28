8e2baa6a15aa3efbdc531f79236be2e757022c78
Rohan Venkatapuram <sharpedo75@gmail.com>
2023-11-27 16:32:03 -0800

    Reverted bad merge

--- src/util/DatetimeUtils.js
+++ src/util/DatetimeUtils.js
@@ -86,103 +86,6 @@ const incrementDateByFrequency = (date, frequency) => {
     }
 };
 
-/* Formates the inputted date as YYYY-MM-DD */
-const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-};
-
-// Will return the date of the beginning of the week (last sunday)
-// and the end of the week (this saturday)
-// Current issue is that if the currentDateString is the start date
-// of a week it will return the previous week end and start date
-const getWeekStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-
-    const weekStartDate = new Date(currentDate);
-    weekStartDate.setDate(currentDate.getDate() - currentDate.getDay());
-
-    const weekEndDate = new Date(currentDate);
-    weekEndDate.setDate(currentDate.getDate() + (6 - currentDate.getDay()));
-
-    return [`${formatDate(weekStartDate)}`, `${formatDate(weekEndDate)}`];
-};
-
-const getPreviousWeekStartEndDate = (inputDate) => {
-    const inputDateObject = new Date(inputDate);
-    const weekPriorDateObject = new Date(inputDateObject);
-    weekPriorDateObject.setDate(inputDateObject.getDate() - 6);
-    return getWeekStartEndDate(formatDate(weekPriorDateObject));
-};
-
-// Current issue is that if the currentDateString is the start date
-// of a week it will return the current week end and start date
-const getNextWeekStartEndDate = (inputDate) => {
-    const inputDateObject = new Date(inputDate);
-    const weekNextDateObject = new Date(inputDateObject);
-    weekNextDateObject.setDate(inputDateObject.getDate() + 6);
-    return getWeekStartEndDate(formatDate(weekNextDateObject));
-};
-
-const getMonthStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-
-    // Get the first day of the month
-    const monthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-
-    // Get the last day of the month
-    const monthEndDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-
-    return [`${formatDate(monthStartDate)}`, `${formatDate(monthEndDay)}`];
-};
-
-const getPreviousMonthStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-
-    // Calculate the first day of the previous month
-    const firstDayOfPreviousMonth = new Date(
-        currentDate.getFullYear(),
-        currentDate.getMonth() - 1,
-        1
-    );
-    // Calculate the last day of the previous month
-    const lastDayOfPreviousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
-    return [`${formatDate(firstDayOfPreviousMonth)}`, `${formatDate(lastDayOfPreviousMonth)}`];
-};
-
-const getNextMonthStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-    currentDate.setMonth(currentDate.getMonth() + 1, 1);
-    const nextMonthStartDate = new Date(currentDate);
-    const nextMonthEndDate = new Date(currentDate);
-    nextMonthEndDate.setMonth(nextMonthEndDate.getMonth() + 1, 0);
-
-    return [`${formatDate(nextMonthStartDate)}`, `${formatDate(nextMonthEndDate)}`];
-};
-
-const getYearStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-    return [`${currentDate.getUTCFullYear()}-01-01`, `${currentDate.getUTCFullYear()}-12-31`];
-};
-
-const getPreviousYearStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-    return [
-        `${currentDate.getUTCFullYear() - 1}-01-01`,
-        `${currentDate.getUTCFullYear() - 1}-12-31`,
-    ];
-};
-
-const getNextYearStartEndDate = (currentDateString) => {
-    const currentDate = new Date(currentDateString);
-    return [
-        `${currentDate.getUTCFullYear() + 1}-01-01`,
-        `${currentDate.getUTCFullYear() + 1}-12-31`,
-    ];
-};
-
 const getDateFromUTCDatetimeString = (datetimeString) => {
     const datetimeParts = TIMESTAMP_REGEX.exec(datetimeString);
     const date = new Date();
@@ -199,15 +102,6 @@ export {
     getCurrentDateString,
     getCurrentUTCTimestamp,
     incrementDateByFrequency,
-    getWeekStartEndDate,
-    getNextWeekStartEndDate,
-    getPreviousWeekStartEndDate,
-    getMonthStartEndDate,
-    getNextMonthStartEndDate,
-    getPreviousMonthStartEndDate,
-    getYearStartEndDate,
-    getPreviousYearStartEndDate,
-    getNextYearStartEndDate,
     getCurrentUTCDatetimeString,
     getDateFromUTCDatetimeString,
     getDateStringFromDate,
