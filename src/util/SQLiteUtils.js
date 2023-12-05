094631525a4f45ebc3280346ab51cc530b9bd78f
Rohan Venkatapuram <sharpedo75@gmail.com>
2023-12-05 14:11:14 -0800

    Added comments for name and sqlite utils

--- src/util/SQLiteUtils.js
+++ src/util/SQLiteUtils.js
@@ -1,3 +1,11 @@
+/**
+ * @module SqliteUtils
+ */
+
+/**
+ * @file Used for storing and generating SQLite commands. Allow separation of JavaScript from SQLite
+ */
+
 import { incrementDateByFrequency } from './DatetimeUtils';
 
 const CREATE_REACCURING_TABLE = `CREATE TABLE IF NOT EXISTS reacurring (
@@ -44,14 +52,37 @@ const GET_ALL_CATEGORIES_QUERY = 'SELECT * FROM categories;';
 
 const GET_ALL_REACURRING_EXPENSES = 'SELECT * FROM reacurring';
 
-const deleteExpense = (row) => {
-    return `DELETE FROM expenses WHERE id = ${row}`;
+/**
+ * Creates a DELETE command that deletes an expense
+ * @param {integer} id - The ID of the expense entry
+ * @return {string} The SQLite DELETE command that deletes that expense
+ */
+const deleteExpense = (id) => {
+    return `DELETE FROM expenses WHERE id = ${id}`;
 };
 
-const createReacurringDeleteById = (row) => {
-    return `DELETE FROM reacurring WHERE id = ${row};`;
+/**
+ * Creates a DELETE command that deletes a recurring expense entry
+ * @param {integer} id - The ID of the recurring expense entry
+ * @return {string} The SQLite DELETE command that deletes that entry
+ */
+const createReacurringDeleteById = (id) => {
+    return `DELETE FROM reacurring WHERE id = ${id};`;
 };
 
+/**
+ * Creates an INSERT command that adds an expense entry
+ * @param {string} name - The name of the expense
+ * @param {string} category - The category of the expense
+ * @param {integer} amount - The amount of the expense
+ * @param {string} timestamp - The timestamp of the expense. Must be in SQLite Timestamp format or a SQLite datetime function call
+ * @param {string} day - The day in local time of the expense in YYYY-MM-DD format
+ * @param {string} subcategory - (optional) The subcategory of the expense
+ * @param {string} picture - (optional) The image path for the expense
+ * @param {string} memo - (optional) The memo of the expense
+ * @param {string} reacurringId - (optional) The reacurring entry id for the expense
+ * @return {string} The SQLite INSERT command that adds that entry
+ */
 const createExpenseInsert = (
     name,
     category,
@@ -61,16 +92,22 @@ const createExpenseInsert = (
     subcategory = null,
     picture = null,
     memo = null,
-    reacurring_id = null
+    reacurringId = null
 ) => {
     return `INSERT INTO expenses (name, category, amount, timestamp, day, subcategory, picture, memo, reacurring_id)
     VALUES ('${name}', ${category}, ${amount}, ${timestamp}, '${day}', ${
         subcategory !== null ? `'${subcategory}'` : null
     }, ${picture !== null ? `'${picture}'` : null}, ${memo !== null ? `'${memo}'` : null}, ${
-        reacurring_id !== null ? `${reacurring_id}` : null
+        reacurringId !== null ? `${reacurringId}` : null
     });`;
 };
 
+/**
+ * Creates an INSERT command that adds an reacurring expense entry
+ * @param {string} start - The start date of the expense as a numerical timestamp in milliseconds
+ * @param {string} frequency - The frequency of the reacurrence. Uses constants from FrequencyConstants.js
+ * @return {string} The SQLite INSERT command that adds that entry
+ */
 const createReacurringInsert = (start, frequency) => {
     const startDate = new Date();
     startDate.setTime(start);
@@ -82,7 +119,14 @@ const createReacurringInsert = (start, frequency) => {
     return command;
 };
 
-const createCategoryInsert = (name, icon = null, color = null) => {
+/**
+ * Creates an INSERT command that adds a category entry
+ * @param {string} name - The name of the category
+ * @param {string} icon - (optional) The icon for the category
+ * @param {string} color - (optional) The color for the category. If not provided, defaults to black
+ * @return {string} The SQLite INSERT command that adds that entry
+ */
+const createCategoryInsert = (name, icon = null, color = 'black') => {
     const command = `INSERT OR IGNORE INTO categories (name, icon, color)
     VALUES ('${name}', ${icon !== null ? `'${icon}'` : null}, ${
         color !== null ? `'${color}'` : null
@@ -90,53 +134,97 @@ const createCategoryInsert = (name, icon = null, color = null) => {
     return command;
 };
 
+/**
+ * Creates a SELECT command that queries for expenses that occur on a given day
+ * @param {string} day- The local time day of the expense in YYYY-MM-DD format
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createExpenseByDayQuery = (day) => {
     return `SELECT * FROM expenses WHERE day = '${day}' ORDER BY timestamp DESC`;
 };
 
+/**
+ * Creates a SELECT command that queries for an expense with a specific id
+ * @param {integer} id- The id of the expense
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createExpenseByIdQuery = (id) => {
     return `SELECT * FROM expenses WHERE id = ${id};`;
 };
 
+/**
+ * Creates a SELECT command that queries for a recurring table entry with a specific id
+ * @param {integer} id- The id of the entry
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createReacurringByIdQuery = (id) => {
     return `SELECT * FROM reacurring WHERE id = ${id};`;
 };
 
+/**
+ * Creates a SELECT command that queries for expenses that occur between two timestamps
+ * @param {integer} startTimestamp- The start timestamp in SQLite Timestamp format
+ * @param {integer} endTimestamp- The end timestamp in SQLite Timestamp format
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createExpenseByTimeframeQuery = (startTimestamp, endTimestamp) => {
     return `SELECT * FROM expenses WHERE timestamp BETWEEN "${startTimestamp}" AND "${endTimestamp}" ORDER BY timestamp`;
 };
 
+/**
+ * Creates a SELECT command that queries for expenses that occur between two days
+ * @param {integer} startTimestamp- The local time start day in YYYY-MM-DD
+ * @param {integer} endTimestamp- The local time end day in YYYY-MM-DD
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createExpenseByDayFrameQuery = (startDay, endDay) => {
     return `SELECT * FROM expenses WHERE DATE(day) BETWEEN '${startDay}' AND '${endDay}' ORDER BY DAY;`;
 };
-/* const createExpenseByTimeframeQuery = (startTimestamp, endTimestamp) => {
-    return `
-        SELECT * 
-        FROM expenses 
-        WHERE timestamp >= "${startTimestamp}" 
-          AND timestamp < date("${endTimestamp}", '+1 day') 
-        ORDER BY timestamp;
-    `;
-}; */
 
+/**
+ * Creates a SELECT command that queries for expenses from a category
+ * @param {integer} category- The id of the category you are querying for
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createExpenseByCategoryQuery = (category) => {
-    return `SELECT * FROM expenses WHERE category = "${category}";`;
+    return `SELECT * FROM expenses WHERE category = ${category};`;
 };
 
-const createReacurringExpenseNextTriggerUpdate = (id, next_update) => {
+/**
+ * Creates a UPDATE command that updates the next time a recurirng expense is supposed to recurr
+ * @param {integer} id- The id of the recurring expense entry
+ * @param {integer} nextUpdate- The numerical timestamp in milliseconds of the next update
+ * @return {string} The SQLite UPDATE command that performs the update
+ */
+const createReacurringExpenseNextTriggerUpdate = (id, nextUpdate) => {
     return `UPDATE reacurring
-    SET next_trigger = datetime(${next_update / 1000}, 'unixepoch')
+    SET next_trigger = datetime(${nextUpdate / 1000}, 'unixepoch')
     WHERE id = ${id};`;
 };
 
+/**
+ * Creates a SELECT command that queries for most recent entry for a recurring expense
+ * @param {integer} id - The id of recurring table entry
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createLastReacurrenceQuery = (id) => {
     return `SELECT * FROM expenses WHERE reacurring_id = ${id} ORDER BY timestamp DESC LIMIT 1;`;
 };
 
+/**
+ * Creates a SELECT command that queries for a category given its name
+ * @param {string} name - The name of the category
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createCategoryQueryByName = (name) => {
     return `SELECT * FROM categories WHERE name = '${name}' LIMIT 1;`;
 };
 
+/**
+ * Creates a SELECT command that queries for a category given its id
+ * @param {integer} id - The id of the category
+ * @return {string} The SQLite SELECT command that performs the query
+ */
 const createCategoryQueryById = (id) => {
     return `SELECT * FROM categories WHERE id = ${id} LIMIT 1;`;
 };
